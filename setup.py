e4a90cf7abe6e563e9b661288339232557b65d94
stewegg <45349707+stewegg@users.noreply.github.com>
2019-01-26 19:33:27 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='gym_4Locations',
+      version='0.0.0',
+      install_requires=['gym>=0.2.3',
+                        'pandas',
+                        'cfg_load']
+      )
